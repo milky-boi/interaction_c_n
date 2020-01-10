dbad153db6cbbd8727ff1bab65caa0fe935a25e7
milky-boi <dr.milojko@gmail.com>
2020-01-10 01:02:50 +0100

    Check for code speed up!

--- distances_to_csv.py
+++ distances_to_csv.py
@@ -7,6 +7,7 @@ import pandas as pd
 import math 
 import time 
 import numpy as np
+import networkx as nx
 
 def distances_f(df1, df2):
     distances = []
@@ -22,46 +23,44 @@ def distances_f(df1, df2):
     return distances
 
 def distances_flies():
-    start_time = time.time()
-    
-    path = r'H:\0_theory\interaction_c_n/data'
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(path):
-        for file in f:
-            if '.csv' in file:
-                files.append(os.path.join(r, file))
-      
-    all_distances = []
-    all_pairs = []
-    
-    for i in range(len(files)):
-        df1 = pd.read_csv(files[i])   
-        next_flie = i + 1
-        
-        if next_flie <= len(files):     
-            for j in range(next_flie, len(files)):
-                df2 = pd.read_csv(files[j])         
-                #globals()['df' + str(i) + '_df' + str(j)] = distances_f(df1, df2)
-                all_distances.append(distances_f(df1, df2))
-                all_pairs.append(str(i) + ' ' + str(j))
-                   
-    df_distances = pd.DataFrame.from_records(all_distances)
-    df_distances.replace(r'^\s*$', np.nan, regex=True)
-    df_distances = df_distances.T
-    df_distances.columns = all_pairs
-    
-    df_distances.to_csv('distances_between_flies.csv')
-    
-    clean_time = time.time()-start_time
-    print("Distances calculated in %.2f" % clean_time + " seconds")
-    print('distances between flies saved to .csv')
+start_time = time.time()
 
+path = r'H:\0_theory\interaction_c_n/data'
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.csv' in file:
+            files.append(os.path.join(r, file))
+  
+all_distances = []
+all_pairs = []
 
+for i in range(len(files)):
+    df1 = pd.read_csv(files[i])   
+    next_flie = i + 1
+    
+    if next_flie <= len(files):     
+        for j in range(next_flie, len(files)):
+            df2 = pd.read_csv(files[j])         
+            #globals()['df' + str(i) + '_df' + str(j)] = distances_f(df1, df2)
+            all_distances.append(distances_f(df1, df2))
+            all_pairs.append(str(i) + ' ' + str(j))
+               
+df = pd.DataFrame.from_records(all_distances)
+df.replace(r'^\s*$', np.nan, regex=True)
+df= df.fillna(999)
+df= df.T
+df.columns = all_pairs
+
+df.to_csv('distances_between_all_flies.csv')
+
+clean_time = time.time()-start_time
+print("Distances calculated in %.2f" % clean_time + " seconds")
+print('distances between all flies saved to .csv')
 
 def main():
     distances_flies()
     
 if __name__ == '__main__':
     main()
-
