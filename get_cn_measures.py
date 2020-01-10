564cb90708bf394829984abaeb21813275fe7c12
milky-boi <dr.milojko@gmail.com>
2020-01-10 02:21:47 +0100

    complete formulas for measures

--- get_cn_measures.py
+++ get_cn_measures.py
@@ -1,5 +1,37 @@
 #loads undirected edgelist
+#number of nodes
+len(G)
+#number of edges
 
-#returns global and loacl measures of graph
+"""measures of prominence"""
+#Degree centrality 
+degree_centrality(G)
+#Degree centralization
 
-#draws bar on line graph of measures
+#eigenvalue centrality.
+eigenvector_centrality(G[, max_iter, tol, …])
+
+#Closeness centrality
+closeness_centrality
+#Closeness centralization
+
+#Betweenness centrality
+betweenness_centrality
+#Betweenness centralization
+
+#Information centrality
+
+"""measures of range"""
+#reach
+#diameter
+diameter(G, e=None)
+"""measures of cohesion"""
+#density
+density(G)
+#reciprocity
+reciprocity(G, nodes=None)
+#clustering coefficient 
+clustering(G[, nodes, weight])
+#fragmentation
+
+#asortivity
